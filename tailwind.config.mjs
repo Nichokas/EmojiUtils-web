16a923fab1453bf98e9ef2a973b05ae03092e9ed
nichokas <nicolas.rodalv@educa.jcyl.es>
2024-12-29 19:58:43 +0100

    Added black theme and logo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
 	theme: {
 		extend: {
 			colors: {
 				apple: {
-					blue: '#0066CC',
-				},
+					blue: {
+						DEFAULT: '#0066CC',
+						dark: '#0A84FF'
+					}
+				}
 			},
 		},
 	},
